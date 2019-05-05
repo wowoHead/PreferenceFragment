a44be0dd18af469942129dbddd9662478900ba84
wowoHead <1031874760@qq.com>
2019-05-05 18:21:08 +0800

    Initial commit

---
+++ app/src/main/java/com/example/ssh/preferencefragment/SettingsActivity.java
@@ -0,0 +1,14 @@
+package com.example.ssh.preferencefragment;
+
+import android.preference.PreferenceActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SettingsActivity extends PreferenceActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.preferences);
+    }
+}
